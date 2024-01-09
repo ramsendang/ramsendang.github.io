4f450cd77286edf58e8533bc6ff8e1c02be80f85
ramsendang <ramsendang02@gmail.com>
2024-01-09 12:28:06 +0000

    initial setup for github and some unfinished codes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Student', 'QA Automation Engineer', 'Web Designer'],
+    typeSpeed: 120,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
