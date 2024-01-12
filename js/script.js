da7c3cb1b19a088263df1a6f546fdcb6b2320a1a
ramsendang <ramsendang02@gmail.com>
2024-01-12 23:58:47 +0000

    worked on responsiveness of navbar

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ const typed = new Typed('.multiple-text', {
 });
 
 
+// for mobile navigation 
+document.getElementById("menu_button").innerHTML = "<i class='fa fa-bars'></i>";
+document.getElementById("menu_button").addEventListener("click", function(){
+    mobile_nav_menu = document.getElementById("mobile_nav_menu");
+    if (mobile_nav_menu.className === "mobile_nav_menu") {
+        mobile_nav_menu.classList.add("responsive");
+        document.getElementById("menu_button").innerHTML = "X";
+    } else {
+        mobile_nav_menu.classList.remove("responsive");
+        document.getElementById("menu_button").style.display = "flex";
+        document.getElementById("menu_button").innerHTML = "<i class='fa fa-bars'></i>";
+    }
+
+});
